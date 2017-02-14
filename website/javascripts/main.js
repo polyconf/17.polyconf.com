0d894ee79e62aa60780680b96b0f65ff60abf11c
Zaiste <oh@zaiste.net>
2017-02-14 17:33:04 +0100

    fix: initialize map with JSONP

--- website/javascripts/main.js
+++ website/javascripts/main.js
@@ -38,21 +38,3 @@ function fixedNav() {
     navi.classList.remove('fixed');
   }
 }
-
-function initMap() {
-
-  var myLatLng = {lat: 48.864716, lng: 2.349014};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 12,
-    styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
-  });
-
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
-}
-
